0ecebd05d2d547bfb8a23682f84f8e50c7dded7c
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2021-11-21 18:27:31 +0300

    Add modified README.md

---
+++ README.md
@@ -0,0 +1,34 @@
+# Name of Project
+##  DELANI_STUDIO
+
+# Author
+##  Agnes Okutoyi
+
+# Description
+##  This is a webpage of Delani Studio which is showcasing their services, how they do their work and also how a user can reach out to them just incase they need they need their services.
+
+# Live link 
+##   
+ 
+
+# Installations
+##  Open terminal
+##  git clone 
+##  cd Akan_names
+##  code . or use atom . based on the text editor you installed.
+
+# BDD
+
+
+# Known Bugs
+###  
+
+# Technologies Used
+###  HTML
+###  CSS
+###  Javascript
+
+# Contact Info
+##  Incase you need to converse with me, do not hesitate to reach out to me via my personal email;
+##  Email ; faithagnes2@gmail.com
+##  Phone ; +254790279917
--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-$(document).ready(function() {
-    $("#design").click(function() {
-        $("#design-").toggle();
-        $("design").toggle();
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design").show("1400");
+        $("#design-image").fadeIn ("1400").hide(1000);
+    });
+    $("#design").click(function(){
+        $("#design").fadeOut("1400");
+        $("#design-image").fadeIn("1400");
+    });
+});
+
+$(document).ready(function(){
+    $("#development-image").click(function(){
+        $("#development").show("1400");
+        $("#development-image").fadeIn ("1400").hide(1000);
+    });
+    $("#development").click(function(){
+        $("#development").fadeOut("1400");
+        $("#development-image").fadeIn("1400");
+    });
+});
+
+$(document).ready(function(){
+    $("#product-image").click(function(){
+        $("#product").show("1400");
+        $("#product-image").fadeIn ("1400").hide(1000);
+    });
+    $("#product").click(function(){
+        $("#product").fadeOut("1400");
+        $("#product-image").fadeIn("1400");
     });
 });
